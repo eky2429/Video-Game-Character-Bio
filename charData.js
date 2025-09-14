56b4a43354a1e22f5dc3b049191a244c63994c3f
Ethan Ky <eky2429@gmail.com>
2025-09-14 08:26:49 -0500

    Fixed bug with character info not displaying for Tony

---
+++ charData.js
@@ -0,0 +1,13 @@
+export const characters = {
+    tony: {
+        name: "Tony Fisher",
+        img_path : "imgs/Tony Fisher.PNG",
+        img_alt : "Tony's portrait",
+        part_id : 1,
+        talent : "Ultimate Lucky Student",
+        likes: "Friends, being Positive",
+        dislikes: "Negativity, Conflict",
+        inspiration: "<a href='https://www.instagram.com/cheroo.chim_comics/'> Cheroo Comics</a>",
+        description: "Tony is a very optimistic kid who likes making friends and encouraging others\nto stay positive. Will this last in the killing game? Let's see..."
+    }
+};
--- script.js
+++ script.js
@@ -1,19 +1,7 @@
-const characters = {
-    tony: {
-        name: "Tony Fisher",
-        img_path : "imgs/Tony Fisher.PNG",
-        img_alt : "Tony's portrait",
-        part_id : 1,
-        talent : "Ultimate Lucky Student",
-        likes: "Friends, being Postiive",
-        dislikes: "Negativity, Conflict",
-        inspiration: "<a href = 'https://www.instagram.com/cheroo.chim_comics/'> Cheroo Comics</a>",
-        description: "Tony is a very omptimistic kid who likes making friends and encouraging others<br />to stay positive. Will this last in the killing game? Let's see..."
-    }
-};
+import { characters} from "./charData.js";
 
 // Function to display the bio
-function showBio(characterKey) {
+export function showBio(characterKey) {
     const char = characters[characterKey];
     const container = document.getElementById("bio-container");
 
@@ -27,7 +15,7 @@ function showBio(characterKey) {
             <h3>${char.name}</h3>
             <img src="${char.img_path}" alt="${char.img_alt}" width="150">
             <ul>
-                <li><b>Participant #${char.part_id.toString()}</b></li>
+                <li><b>Participant #${char.part_id ? char.part_id.toString() : "N/A"}</b></li>
                 <li><b>Talent:</b>${char.talent}</li>
                 <li><b>Interests:</b> ${char.likes}</li>
                 <li><b>Dislikes:</b> ${char.dislikes}</li>
@@ -37,3 +25,12 @@ function showBio(characterKey) {
         </div>
     `;
 }
+
+// Make showBio available globally
+window.showBio = showBio;
+
+//Adds event listeners to buttons
+document.getElementById('tony').addEventListener('click', () => showBio('tony'));
+document.getElementById('eric').addEventListener('click', () => showBio('eric'));
+document.getElementById('ken').addEventListener('click', () => showBio('ken'));
+document.getElementById('amelia').addEventListener('click', () => showBio('amelia'));
