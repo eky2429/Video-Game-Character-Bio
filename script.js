af152df25c0d93d9212f910209be17d447975958
Ethan Ky <eky2429@gmail.com>
2025-09-14 19:01:39 -0500

    Added modal window

--- script.js
+++ script.js
@@ -3,28 +3,41 @@ import { characters} from "./charData.js";
 // Function to display the bio
 export function showBio(characterKey) {
     const char = characters[characterKey];
-    const container = document.getElementById("bio-container");
+    const modal = document.getElementById("bio-modal");
+    const container = document.getElementById("modal-bio-container");
 
     if (!char) {
         container.innerHTML = "<p>Character not found.</p>";
-        return;
+    } else {
+        container.innerHTML = `
+            <div class="character-card">
+                <h3>${char.name}</h3>
+                <img src="${char.img_path}" alt="${char.img_alt}" width="150">
+                <ul>
+                    <li><b>Participant #${char.part_id ? char.part_id.toString() : "N/A"}</b></li>
+                    <li><b>Talent:</b> ${char.talent}</li>
+                    <li><b>Interests:</b> ${char.likes}</li>
+                    <li><b>Dislikes:</b> ${char.dislikes}</li>
+                    <li><b>Inspired by:</b> ${char.inspiration}</li>
+                </ul>
+                <p><b>Description:</b> ${char.description}</p>
+            </div>
+        `;
     }
-
-    container.innerHTML = `
-        <div class="character-card">
-            <h3>${char.name}</h3>
-            <img src="${char.img_path}" alt="${char.img_alt}" width="150">
-            <ul>
-                <li><b>Participant #${char.part_id ? char.part_id.toString() : "N/A"}</b></li>
-                <li><b>Talent:</b>${char.talent}</li>
-                <li><b>Interests:</b> ${char.likes}</li>
-                <li><b>Dislikes:</b> ${char.dislikes}</li>
-                <li><b>Inspired by:</b> ${char.inspiration}</li>
-            </ul>
-            <p><b>Description:</b> ${char.description}</p>
-        </div>
-    `;
+    modal.style.display = "flex";
 }
+
+// Close modal on button click
+document.getElementById('close-modal').addEventListener('click', () => {
+    document.getElementById('bio-modal').style.display = "none";
+});
+
+// Optional: Close modal when clicking outside modal content
+document.getElementById('bio-modal').addEventListener('click', (e) => {
+    if (e.target.id === 'bio-modal') {
+        document.getElementById('bio-modal').style.display = "none";
+    }
+});
 
 // Make showBio available globally
 window.showBio = showBio;
